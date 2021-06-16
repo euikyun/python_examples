29272074224e3ebf3734e867f8c7818c7ddb602f
euikyun <euikyun@korea.ac.kr>
2021-06-16 14:38:50 +0900

    0616

--- 알고리즘/2.구현/3.상하좌우 문제.py
+++ 알고리즘/2.구현/3.상하좌우 문제.py
@@ -1,17 +1,19 @@
 import sys
-n=int(input())
+n=int(sys.stdin.readline().strip())
 
-data1 = list(map(int,sys.stdin.readline().split()))
-# data2 = sys.stdin.readline().split()
-print(data1)
+dir = sys.stdin.readline().split()
+x,y=1,1
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
 
-# x,y=1,1
-# dx=[0,0,1,-1]
-# dy=[1,-1,0,0]
+move=['R','L','D','U']
+for i in dir:
+    for j in range(len(move)):
+        if (i == move[j]):
+            nx=x+dx[j]
+            ny=y+dy[j]
+        if nx<1 or ny<1 or nx > n or ny > n:
+            continue
+        x,y=nx,ny
+    print('(',x,',',y,')')
 
-# move=['R','L','D','U']
-
-# for i in range(len(move)):
-#     nx=x+dx[i]
-#     ny=y+dy[i]
-#     print('(',nx,',',ny,')')

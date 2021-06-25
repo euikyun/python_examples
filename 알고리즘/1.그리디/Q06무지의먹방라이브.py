bdfc748d814929e6d8deb4dd3bbeb4e7c587c4c5
Euikyun Choi <79953557+euikyun@users.noreply.github.com>
2021-06-25 10:48:06 +0900

    Update Q06무지의먹방라이브.py

--- 알고리즘/1.그리디/Q06무지의먹방라이브.py
+++ 알고리즘/1.그리디/Q06무지의먹방라이브.py
@@ -1,16 +1,30 @@
 import sys
-data = list(map(int,input().split()))    #int
-k=int(sys.stdin.readline().strip())
 
-def solution(data, k):
-    for i in range(1,k+1):
-        if data[i%len(data)-1]!=0:
-            data[i%len(data)-1]-=1
-        
+food_times=list(map(int,input().split()))
+k=int(sys.stdin.readline())
 
-    answer = k%len(data)-1
-    return answer
+def solution(food_times,k):
+  foods=[]
+  n=len(food_times)
+  for i in range(n):
+    foods.append((food_times[i],i+1))
+  print(foods)
+  foods.sort()
+  print(foods)
+  pretime=0
+  for i, food in enumerate(foods):
+    print(food[0])
+    diff=food[0]-pretime
+    if diff!=0:
+      spend=diff*n
+      if spend <=k:
+        k-=spend
+        pretime=food[0]
+      else:
+        k%=n
+        sublist=sorted(foods[i:],key=lambda x:x[1])
+        return sublist[k][1]
+    n-=1
+  return -1
 
-res=solution(data,k)
-print(res)
-print(data)
+print("정답 = ",solution(food_times,k))

dc53441841ef13fe5ec0e342be5d915b17d2af56
euikyun <euikyun@korea.ac.kr>
2022-01-31 12:06:28 +0900

    인프런 연습

---
+++ 0. inflearn/1변수.py
@@ -0,0 +1,13 @@
+#애완동물을 소개해 주세요
+animal="고양이"
+name ="연탄이"
+age=4
+hobby="산책"
+is_adult=age>=3
+
+print("우리집 "+animal+"의 이름은"+name+"이에요")
+#print(name+"는"+str(age)+"살이며")
+print(name,"는",age,"살이며")
+print(name + "는 어른일까요?"+str(is_adult))
+
+
---
+++ 0. inflearn/2연산자.py
@@ -0,0 +1,30 @@
+# print(1+1)
+# print(2**3)
+# print(5%3)
+# print(5//3)
+# print(10//3)
+# print(4 >=7)  
+# print(1!=3)
+# print(not 1!=2)
+# print((3>0)and(3<5))
+# print((3>0)&(3<5))
+# print((3>0)or(3>5))
+# print((3>0)|(3>5))
+
+# print(pow(4,2))
+# print(max(5,12))
+# print(round(4.1234,2))
+
+# from math import *
+# print(floor(4.99))
+# print(ceil(3.14))
+
+
+from random import *
+# print(random()) # 0~1사이 임의의 값
+# print(int(random()*10)) # 0이상1미만 임의의 값
+# print(int(random()*10)) # 0이상10미만 임의의 값
+# print(int(random()*10)+1) # 1이상10이하 임의의 값
+# print(int(random()*45) # 0이상45미만 임의의 값
+print(randrange(1,45)) #1이상 45미만 임의의 값
+print(randint(1,45))
